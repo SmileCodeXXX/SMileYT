8d67679ff60aef1ed3ecd5a69079d1c9a6388a2f
Solomon Cammue <s.cammue@gmail.com>
2022-12-28 06:00:31 -0600

    added google ads in header

--- src/conponent/AdsPage.jsx
+++ src/conponent/AdsPage.jsx
@@ -6,18 +6,15 @@ const AdSense = () => {
      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1245818362998975"
      crossorigin="anonymous"></script>
       <ins
-        class={adsbygoogle}
-        style={{ display: 'block', width: 100 }}
+        class="adsbygoogle"
+        style={{ display: 'inline-block', width: 100 }}
         data-ad-client="ca-pub-1245818362998975"
         data-ad-slot="5510111893"
         
         data-full-width-responsive="true"
       ></ins>
       <script>
-     { window.onload = function() {
-    (adsbygoogle = window.adsbygoogle || []).push({})
-        }}
-
+        (adsbygoogle = window.adsbygoogle || []).push({})
       </script>
     </div>
   );
