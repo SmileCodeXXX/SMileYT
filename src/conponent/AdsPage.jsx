243e4138ade9f2de8849f862c5415cf19a0925c1
Solomon Cammue <s.cammue@gmail.com>
2022-12-28 03:21:50 -0600

    added google ads in header

---
+++ src/conponent/AdsPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AdSense = () => {
+  return (
+    <div>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1245818362998975"
+        crossorigin="anonymous"
+      ></script>
+
+      <ins
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-1245818362998975"
+        data-ad-slot="5510111893"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
+  );
+};
+
+export default AdSense;
--- src/conponent/Main.jsx
+++ src/conponent/Main.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Card from './Card';
 import Axios from 'axios'
+import AdSense from './AdsPage';
 
 function Main() {
   const[getURL, setGetURL] = useState('');
@@ -49,7 +50,7 @@ function Main() {
             </form>
           
         </div>
-
+    <AdSense/>
         <div className="download-list">
             {
                 getDatas.map((data)=>{
