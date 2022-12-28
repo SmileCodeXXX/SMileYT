45828d3041b80be9c2aa4eff196f581c7fceaa57
Solomon Cammue <s.cammue@gmail.com>
2022-12-28 05:17:09 -0600

    added google ads in header

--- src/conponent/AdsPage.jsx
+++ src/conponent/AdsPage.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 const AdSense = () => {
   return (
     <div>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
+     
       <ins
         class="adsbygoogle"
         style={{ display: 'block' }}
-        data-ad-client="ca-pub-123456789"
-        data-ad-slot="1234567890"
+        data-ad-client="ca-pub-1245818362998975"
+        data-ad-slot="5510111893"
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
