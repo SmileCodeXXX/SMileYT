447d0b7995ad84c553f76e6b66fd89f968457770
Solomon Cammue <s.cammue@gmail.com>
2022-12-28 12:30:57 -0600

    added google ads in header

--- src/conponent/AdsPage.jsx
+++ src/conponent/AdsPage.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
 const AdSense = () => {
-  //let adsbygoogle;
+  useEffect(() => {
+    window.adsbygoogle = window.adsbygoogle || []
+    window.adsbygoogle.push({})
+  })
   return (
     <div>
       <ins
         class="adsbygoogle"
-        style={{ display: 'block', zIndex:'4', width:500, border:'1px solid red'}}
+        style={{ display: 'block', zIndex:'4', width:'728px', height:'90px'}}
         data-ad-client="ca-pub-1245818362998975"
         data-ad-slot="5510111893"
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
-      <script>
-        
-            {(adsbygoogle = window.adsbygoogle || []).push({})}
-     
-      </script>
     </div>
   );
 };
