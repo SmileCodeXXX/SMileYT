de4a90942e4e2eb07f3d1e69950da94fa50638ac
Solomon Cammue <s.cammue@gmail.com>
2022-12-28 12:57:32 -0600

    added google ads in header

--- src/conponent/AdsPage.jsx
+++ src/conponent/AdsPage.jsx
@@ -1,15 +1,16 @@
 import React,{useEffect} from 'react';
 
-const AdSense = () => {
+const AdSense = props => {
+  const { currentPath } = props
   useEffect(() => {
     window.adsbygoogle = window.adsbygoogle || []
     window.adsbygoogle.push({})
   },[])
   return (
-    <div>
+    <div key={currentPath}>
       <ins
         class="adsbygoogle"
-        style={{ display: 'block', zIndex:'4', width:'728px', height:'90px'}}
+        style={{ display: 'block', zIndex:'4', width:'500px', height:'90px'}}
         data-ad-client="ca-pub-1245818362998975"
         data-ad-slot="5510111893"
       />
